69fc85cc11dbacf4e0cfa8e323d0d3e22bbecf64
Ethan <3laverack@gmail.com>
2020-01-18 17:18:28 -0500

    created basic functional components for page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Teamalyzer from "./components/Teamalyzer";
+import Footer from "./components/Footer";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Teamalyzer />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Teamalyzer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Teamalyzer() {
+  return <div>teamalyzer</div>;
+}
+
+export default Teamalyzer;
