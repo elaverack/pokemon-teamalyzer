b93d4920371aaef5b409fae2cbba89ce0265c894
Ethan <3laverack@gmail.com>
2020-01-23 23:49:08 -0500

    todo notes in import component

--- src/components/ImportExport.js
+++ src/components/ImportExport.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+/* TODO
+create functions needed to convert showdown export into pokemon class
+update the team state using the inputted pokemon
+don't store sets like in the damage calc? do users want to rapidly switch between potential sets/spreads? STRETCH FEATURE
+what happens when someone inputs less than 6 pokemon, who gets replaced? probably last pokemon gets replaced
+user selects which pokemon to replace? STRETCH FEATURE
+*/
+
 class ImportExport extends React.Component {
   render() {
     return (
