452c8ab01de8f44f39580d43e2fd06ae0e3f954e
Wheaties <3laverack@gmail.com>
2020-05-09 11:28:26 -0400

    fixed table render by wrapping table in function

--- src/components/tools/OffensiveTable.js
+++ src/components/tools/OffensiveTable.js
@@ -2,27 +2,7 @@ import React from 'react';
 import { useTable } from 'react-table';
 
 function OffensiveTable(props) {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: 'Hello',
-        col2: 'World',
-      },
-      {
-        col1: 'react-table',
-        col2: 'rocks',
-      },
-      {
-        col1: 'whatever',
-        col2: 'you want',
-      },
-    ],
-    []
-  );
-
-  const data1 = React.useMemo(() => getOffenseData(), []);
-
-  function getOffenseData() {
+  function genData() {
     const data = [
       {
         col1: 'Hello',
@@ -40,48 +20,60 @@ function OffensiveTable(props) {
     return data;
   }
 
-  const columns = React.useMemo(() => [
-    {
-      Header: 'Type 1',
-      accessor: 'col1', // accessor is the "key" in the data
-    },
-    {
-      Header: 'Type 2',
-      accessor: 'col2',
-    },
-  ]);
+  function genColumns() {
+    return [
+      {
+        Header: 'Type 1',
+        accessor: 'col1',
+      },
+      {
+        Header: 'Type 2',
+        accessor: 'col2',
+      },
+    ];
+  }
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data1 });
-  console.log(data);
-  console.log(getOffenseData(props.teamState));
+  const columns = React.useMemo(() => genColumns(), []);
+
+  const data = React.useMemo(() => genData(), []);
 
   return (
     <div>
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Table columns={columns} data={data} />
     </div>
   );
+
+  function Table({ columns, data }) {
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+    return (
+      <div>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default OffensiveTable;
